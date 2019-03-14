46452595802f9eb15c797ece359ea310e16b467d
sasha <smatur98@gmail.com>
2019-03-14 22:36:59 +0200

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minificator URL</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Сокращение URL</h1>
+        <form action="short.php" method="post">
+            <input type="url" name="url" placeholder="Вставьте сюда URL" autocomplete="off">
+            <input type="submit" value="Сократить" >
+        </form>
+    </div>
+</body>
+</html>
